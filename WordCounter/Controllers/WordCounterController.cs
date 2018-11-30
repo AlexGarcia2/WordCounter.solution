e6e9b69c4932d2be035b4024fc015345ec1923fb
Alex Garcia <g.nightster11@gmail.com>
2018-11-30 14:58:49 -0800

    working on views files . Atom is not seeing html code in views file

--- WordCounter/Controllers/WordCounterController.cs
+++ WordCounter/Controllers/WordCounterController.cs
@@ -10,7 +10,7 @@ namespace WordCounter.Controllers
     public ActionResult Index()
     {
 
-      return View("people");
+      return View();
     }
     [HttpGet("/wordCounter/new")]
     public ActionResult New()
